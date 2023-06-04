4a097c0acee3f8018b2bfce7f5c6fed79ea164ba
Ihar Kuiko <coooll81@gmail.com>
2023-06-04 20:57:20 +0200

    add model Category and Posts

--- rcbs/posts/models.py
+++ rcbs/posts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+class Posts(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    description = models.CharField(max_length=256)
+    text = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    create_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    is_published = models.BooleanField()
+    image = models.ImageField()
+
